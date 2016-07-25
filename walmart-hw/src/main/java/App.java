59df4a9431cae420eeebe6b38d68ae8903d663da
Russell Chase <russellmchase@gmail.com>
2016-07-25 12:40:41 -0500

    converted to eclipse console app

--- walmart-hw/src/main/java/App.java
+++ walmart-hw/src/main/java/App.java
@@ -1,6 +1,6 @@
-import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 import Deserializers.WalmartClient;
 import models.RecommendationProduct;
@@ -12,21 +12,10 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		/*int custInputSize = args.length;
-		String query = "&query=";
-		
-		//concat all of the cmd line arguments
-		for(int i = 0; i < custInputSize; i++){
-			query = query + args[i] + " ";
-		}*/
+		Scanner in = new Scanner(System.in);
 		String query = "&query=";
 		System.out.print("Please enter your search: ");
-		try {
-			query = query + System.in.read();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		query = query + in.next();
 		
 		
 		//build a query string. the customer input is a cmd line argument.
@@ -35,7 +24,7 @@ public class App {
 		final String recommendApiUrl = "http://api.walmartlabs.com/v1/nbp?";
 		final String format = "&format=json";
 		final String searchUrl = searchApiUrl + apiKey + query + format;
-		
+				
 		final String searchResults;
 		final String recommendResults;
 		
